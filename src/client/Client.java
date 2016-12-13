2584b16378a42c68a7609816c176878b009d43d4
Tobias Gillström <tejb54@gmail.com>
2016-12-13 16:00:19 +0100

    Basic implementation of client movment.

--- src/client/Client.java
+++ src/client/Client.java
@@ -1,5 +1,6 @@
 package client;
 
+import com.sun.javafx.geom.Vec2f;
 import server.Agent;
 import server.SimpleAgent;
 import shared.ClientResponse;
@@ -30,6 +31,51 @@ public class Client {
 
     static float angle = 0;
 
+    public static Vec2f calcAlignment(ServerResponse response)
+    {
+        double length = 1;
+        double x = 0;
+        double y = 0;
+
+        for(int i = 0; i < response.neighbors.length; i++)
+        {
+            SimpleAgent currentAgent = response.neighbors[i];
+            x += currentAgent.xVelocity;
+            y += currentAgent.yVelocity;
+        }
+
+        x/=response.neighbors.length;
+        y/=response.neighbors.length;
+
+        length = Math.sqrt(Math.pow(x,2) + Math.pow(y,2));
+
+        return new Vec2f((float) (x/length),(float)(y/length));
+    }
+
+    static public Vec2f calcCohesion(ServerResponse response)
+    {
+        double length = 1;
+        double x = 0;
+        double y = 0;
+
+        for(int i = 0; i < response.neighbors.length; i++)
+        {
+            SimpleAgent currentAgent = response.neighbors[i];
+            x += currentAgent.xPos;
+            y += currentAgent.yPos;
+        }
+
+        x/=response.neighbors.length;
+        y/=response.neighbors.length;
+
+        x = x - response.xPos;
+        y = y - response.yPos;
+
+        length = Math.sqrt(Math.pow(x,2) + Math.pow(y,2));
+
+        return new Vec2f((float) (x/length),(float)(y/length));
+    }
+
     private static void runClient() throws Exception
     {
         Socket clientSocket = new Socket(InetAddress.getByName("localhost"),1234);
@@ -44,7 +90,14 @@ public class Client {
         clientParser.registerCallback("getAction",(String header, Object payload)->{
             ServerResponse response = (ServerResponse)payload;
             System.out.println(response.neighbors.length);
-            clientParser.sendData(new CommunicationObj("basic",new ClientResponse( (float)Math.cos(angle),(float)Math.sin(angle))));
+
+            Vec2f vA = calcAlignment(response);
+            Vec2f vC = calcCohesion(response);
+
+
+
+                clientParser.sendData(new CommunicationObj("basic",new ClientResponse( (float)Math.cos(angle),(float)Math.sin(angle))));
+
         });
 
 
--- src/server/SimpleAgent.java
+++ src/server/SimpleAgent.java
@@ -7,10 +7,10 @@ import java.util.List;
  * Created by Tobias on 2016-12-12.
  */
 public class SimpleAgent implements Serializable {
-    double xPos; //this is used by the GUI.
-    double yPos; //this is used by the GUI.
-    double xVelocity;
-    double yVelocity;
+    public double xPos; //this is used by the GUI.
+    public double yPos; //this is used by the GUI.
+    public double xVelocity;
+    public double yVelocity;
 
     public SimpleAgent(double xPos, double yPos, double xVelocity, double yVelocity) {
         this.xPos = xPos;
